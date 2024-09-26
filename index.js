3a5a2f2f4a89ec57bf17f033a95be831865fa352
h0gm4n <atte.hogman@gmail.com>
2024-09-26 16:10:32 +0300

    3.2

--- index.js
+++ index.js
@@ -32,6 +32,10 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    response.send(`<div>Phonebook has info for ${persons.length}</div><div>${Date()}</div>`)
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
